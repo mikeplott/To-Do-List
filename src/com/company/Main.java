0402724d6b1c60af2332d496f2392886dd5cbe82
michaelplott <mike.plott80@gmail.com>
2016-09-19 11:33:34 -0400

    added user login and logout

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,55 +1,73 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Item> items = new ArrayList<>();
+        HashMap<String, ArrayList<Item>> users = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
 
-        while (true) {
+        while(true) {
             System.out.println();
-            System.out.println("1: Create to-do item");
-            System.out.println("2: Toggle to-do item");
-            System.out.println("3: List all to-do items");
-            System.out.println();
+            System.out.println("Please enter your username");
+            String name = scanner.nextLine();
 
-            String option = scanner.nextLine();
+            ArrayList<Item> items = users.get(name);
+            if (items == null) {
+                items = new ArrayList<>();
+                users.put(name, items);
+            }
+            boolean isLoggedIn = true;
+            while (isLoggedIn) {
+                System.out.println();
+                System.out.println("1: Create to-do item");
+                System.out.println("2: Toggle to-do item");
+                System.out.println("3: List all to-do items");
+                System.out.println("4: Logout");
+                System.out.println();
 
-            switch (option) {
-                case "1":
-                    System.out.println();
-                    System.out.println("Enter your to-do item:");
-                    String text = scanner.nextLine();
-                    Item item = new Item(text, false);
-                    items.add(item);
-                    break;
-                case "2":
-                    System.out.println();
-                    System.out.println("Which item do you want to toggle?");
-                    int i = Integer.valueOf(scanner.nextLine());
-                    Item item2 = items.get(i -1);
-                    item2.isDone = !item2.isDone;
-                    break;
-                case "3":
-                    System.out.println();
-//                    for (Item item3 : items) {
-//                        System.out.println(item3.text);
-//                    }
-                    for (int j = 0; j < items.size(); j++ ) {
-                        Item item3 = items.get(j);
-                        int num = j +1;
-                        String checkbox = "[ ]";
-                        if (item3.isDone) {
-                            checkbox = "[x]";
+                String option = scanner.nextLine();
+
+                switch (option) {
+                    case "1":
+                        System.out.println();
+                        System.out.println("Enter your to-do item:");
+                        String text = scanner.nextLine();
+                        Item item = new Item(text, false);
+                        items.add(item);
+                        break;
+                    case "2":
+                        System.out.println();
+                        System.out.println("Which item do you want to toggle?");
+                        int i = Integer.valueOf(scanner.nextLine());
+                        Item item2 = items.get(i -1);
+                        item2.isDone = !item2.isDone;
+                        break;
+                    case "3":
+                        System.out.println();
+    //                    for (Item item3 : items) {
+    //                        System.out.println(item3.text);
+    //                    }
+                        for (int j = 0; j < items.size(); j++ ) {
+                            Item item3 = items.get(j);
+                            int num = j +1;
+                            String checkbox = "[ ]";
+                            if (item3.isDone) {
+                                checkbox = "[x]";
+                            }
+                            System.out.println(checkbox + " " + num + ":" + " " + item3.text);
                         }
-                        System.out.println(checkbox + " " + num + ":" + " " + item3.text);
-                    }
-                    break;
-                default:
-                    System.out.println("Invalid option.");
+                        break;
+                    case "4" :
+                        isLoggedIn = false;
+                        //main(args);
+                        break;
+                    default:
+                        System.out.println("Invalid option.");
+                }
             }
         }
     }
