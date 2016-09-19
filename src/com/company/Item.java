5fd836e3eeeeb069613b643112d77fa8d99bdc2e
michaelplott <mike.plott80@gmail.com>
2016-09-19 11:16:09 -0400

    Initial push

---
+++ src/com/company/Item.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by michaelplott on 9/19/16.
+ */
+public class Item {
+    String text;
+    boolean isDone;
+
+    public Item(String text, boolean isDone) {
+        this.text = text;
+        this.isDone = isDone;
+
+
+    }
+}
