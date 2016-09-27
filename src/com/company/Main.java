6768344c0f9a4c9d3f9cb0801da776888b40b7ec
michaelplott <mike.plott80@gmail.com>
2016-09-27 09:42:31 -0400

    refactored into seperate methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.sun.tools.javac.jvm.Items;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -14,7 +16,6 @@ public class Main {
             System.out.println();
             System.out.println("Please enter your username");
             String name = scanner.nextLine();
-
             ArrayList<Item> items = users.get(name);
             if (items == null) {
                 items = new ArrayList<>();
@@ -28,48 +29,53 @@ public class Main {
                 System.out.println("3: List all to-do items");
                 System.out.println("4: Logout");
                 System.out.println();
-
                 String option = scanner.nextLine();
-
                 switch (option) {
                     case "1":
-                        System.out.println();
-                        System.out.println("Enter your to-do item:");
-                        String text = scanner.nextLine();
-                        Item item = new Item(text, false);
-                        items.add(item);
+                        addToDo(scanner, items);
                         break;
                     case "2":
-                        System.out.println();
-                        System.out.println("Which item do you want to toggle?");
-                        int i = Integer.valueOf(scanner.nextLine());
-                        Item item2 = items.get(i -1);
-                        item2.isDone = !item2.isDone;
+                        toggleToDo(scanner, items);
                         break;
                     case "3":
-                        System.out.println();
-    //                    for (Item item3 : items) {
-    //                        System.out.println(item3.text);
-    //                    }
-                        for (int j = 0; j < items.size(); j++ ) {
-                            Item item3 = items.get(j);
-                            int num = j +1;
-                            String checkbox = "[ ]";
-                            if (item3.isDone) {
-                                checkbox = "[x]";
-                            }
-//                            System.out.println(checkbox + " " + num + ":" + " " + item3.text);
-                            System.out.printf("%s  %s. %s\n", checkbox, num, item3.text);
-                        }
+                        printList(items);
                         break;
                     case "4" :
                         isLoggedIn = false;
-                        //main(args);
                         break;
                     default:
                         System.out.println("Invalid option.");
                 }
             }
+        }
+    }
+
+    public static void addToDo (Scanner scanner, ArrayList<Item> items) {
+        System.out.println();
+        System.out.println("Enter your to-do item:");
+        String text = scanner.nextLine();
+        Item item = new Item(text, false);
+        items.add(item);
+    }
+
+    public static void toggleToDo(Scanner scanner, ArrayList<Item> items) {
+        System.out.println();
+        System.out.println("Which item do you want to toggle?");
+        int i = Integer.valueOf(scanner.nextLine());
+        Item item2 = items.get(i -1);
+        item2.isDone = !item2.isDone;
+    }
+
+    public static void printList(ArrayList<Item> items) {
+        System.out.println();
+        for (int j = 0; j < items.size(); j++ ) {
+            Item item3 = items.get(j);
+            int num = j +1;
+            String checkbox = "[ ]";
+            if (item3.isDone) {
+                checkbox = "[x]";
+            }
+            System.out.printf("%s  %s. %s\n", checkbox, num, item3.text);
         }
     }
 }
